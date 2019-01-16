e18c5ffb1376785789d2a2b4341f02acffdb7a07
Alex Broaddus <alexbroaddus1@gmail.com>
2019-01-16 13:58:16 -0500

    Added setup.py and moved banweb.py to package folder

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='banweb',
+    version='0.1',
+    description='Interface with the banner website to access academic information'
+    url='https://github.com/Alex979/banweb-python#loginroot-sid-pin-security_answer'
+    license='MIT'
+    packages=['banweb']
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ],
+    zip_safe=False)
