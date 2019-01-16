7320b7c79a9f21403662c74ff6dd520f30fc6e91
Alex Broaddus <alexbroaddus1@gmail.com>
2019-01-16 14:01:11 -0500

    Fixed url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='banweb',
     version='0.1',
     description='Interface with the banner website to access academic information'
-    url='https://github.com/Alex979/banweb-python#loginroot-sid-pin-security_answer'
+    url='https://github.com/Alex979/banweb-python'
     license='MIT'
     packages=['banweb']
     install_requires=[
